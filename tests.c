a9234be0233c2764e376ddd8038e62af5e7217f0
clairelsr <34139274+clairelsr@users.noreply.github.com>
2017-11-30 20:44:53 +0100

    Add files via upload

---
+++ tests.c
@@ -0,0 +1,118 @@
+/* Testing Code */
+
+#include <limits.h>
+#include <math.h>
+
+/* Routines used by floation point test code */
+
+/* Convert from bit level representation to floating point number */
+float u2f(unsigned u) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.u = u;
+  return a.f;
+}
+
+/* Convert from floating point number to bit-level representation */
+unsigned f2u(float f) {
+  union {
+    unsigned u;
+    float f;
+  } a;
+  a.f = f;
+  return a.u;
+}
+
+/* Copyright (C) 1991-2012 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* We do support the IEC 559 math functionality, real and complex.  */
+/* wchar_t uses ISO/IEC 10646 (2nd ed., published 2011-03-15) /
+   Unicode 6.0.  */
+/* We do not support C11 <threads.h>.  */
+/* bit manipulation */
+int test_isZero(int x) {
+  return x == 0;
+}
+int test_bitNor(int x, int y)
+{
+  return ~(x|y);
+}
+int test_upperBits(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < x; i++)
+    result |= (1 << (31-i));
+  return result;
+}
+int test_copyLSB(int x)
+{
+  return (x & 0x1) ? -1 : 0;
+}
+int test_allEvenBits(int x) {
+  int i;
+  for (i = 0; i < 32; i+=2)
+      if ((x & (1<<i)) == 0)
+   return 0;
+  return 1;
+}
+int test_logicalShift(int x, int n) {
+  unsigned u = (unsigned) x;
+  unsigned shifted = u >> n;
+  return (int) shifted;
+}
+int test_bitParity(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i++)
+    result ^= (x >> i) & 0x1;
+  return result;
+}
+/* 2's complement */
+int test_tmin(void) {
+  return 0x80000000;
+}
+int test_isTmax(int x) {
+    return x == 0x7FFFFFFF;
+}
+int test_negate(int x) {
+  return -x;
+}
+int test_addOK(int x, int y)
+{
+    long long lsum = (long long) x + y;
+    return lsum == (int) lsum;
+}
+int test_isPositive(int x) {
+  return x > 0;
+}
+int test_satAdd(int x, int y)
+{
+  if (x > 0 && y > 0 && x+y < 0)
+    return (0x7FFFFFFF);
+  if (x < 0 && y < 0 && x+y >= 0)
+    return (0x80000000);
+  return x + y;
+}
